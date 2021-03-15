52c7b0fcf07fec81d34f47922471b5888c8719fa
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-15 23:13:10 +0900

    Commit unary operators

--- __tests__/operands_test.js
+++ __tests__/operands_test.js
@@ -3,14 +3,18 @@ import {
   substract,
   multiply,
   modulusCalc,
-  raise
+  raise,
+  plus,
+  minus,
+  increment,
+  decrement
 } from "../lib/operands";
 
 // Binary operators
 test("Addition", () => {
     const sum_1 = add(100, 200);
-    const sum_2 = add(10, 0.5)
-    const str  = add("Java", "Script")
+    const sum_2 = add(10, 0.5);
+    const str  = add("Java", "Script");
     expect(sum_1).toBe(300);
     expect(sum_2).toBe(10.5);
     expect(str).toBe("JavaScript");
@@ -47,4 +51,43 @@ test("Modulus calculation", () => {
 test("Raise", () => {
     const val = raise(4, 4);
     expect(val).toBe(256);
+});
+
+// Unary operators
+test("Plus operator", () => {
+    const val_1 = plus("100");
+    const val_2 = plus("String");
+    expect(val_1).toBe(100);
+    expect(Number.isNaN(val_2)).toBe(true);
+});
+
+test("Minus operator", () => {
+    const val_1 = minus("100");
+    const val_2 = minus("String");
+    expect(val_1).toBe(-100);
+    expect(Number.isNaN(val_2)).toBe(true);
+});
+
+test("Incrementation", () => {
+    const vals = increment(1);
+    const val_1 = vals[0];
+    const num_1 = vals[1];
+    const val_2 = vals[2];
+    const num_2 = vals[3];
+    expect(val_1).toBe(1);
+    expect(num_1).toBe(2);
+    expect(val_2).toBe(2);
+    expect(num_2).toBe(2);
+});
+
+test("Decrementation", () => {
+    const vals = decrement(1);
+    const val_1 = vals[0];
+    const num_1 = vals[1];
+    const val_2 = vals[2];
+    const num_2 = vals[3];
+    expect(val_1).toBe(1);
+    expect(num_1).toBe(0);
+    expect(val_2).toBe(0);
+    expect(num_2).toBe(0);
 });
