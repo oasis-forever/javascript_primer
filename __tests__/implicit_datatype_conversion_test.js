1a3e1f57f10de8182c7ac64d8386b5869ae65073
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-17 10:57:49 +0900

    Commit logical equal

--- __tests__/implicit_datatype_conversion_test.js
+++ __tests__/implicit_datatype_conversion_test.js
@@ -1,5 +1,6 @@
 import {
   addBoolean,
+  logicalEqual
 } from "../lib/implicit_datatype_conversion";
 
 // Num plus boolean
@@ -10,3 +11,16 @@ test("Num plus boolean", () => {
   expect(sum_2).toBe(1);
 });
 
+// Logical equal
+test("Logical equal", () => {
+  const result_1 = logicalEqual(1, true);
+  const result_2 = logicalEqual(0, false);
+  const result_3 = logicalEqual(100, "100");
+  const result_4 = logicalEqual(100, [100]);
+  const result_5 = logicalEqual(100, ["100"]);
+  expect(result_1).toBe(true);
+  expect(result_2).toBe(true);
+  expect(result_3).toBe(true);
+  expect(result_4).toBe(true);
+  expect(result_5).toBe(true);
+});
