42c46ea261c03878d717b15cfa0c744b30233a6a
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-18 12:48:17 +0900

    Rename function

--- __tests__/conditional_statements_test.js
+++ __tests__/conditional_statements_test.js
@@ -1,13 +1,13 @@
 import {
-  getJavaScriptVersion
+  getJavaScriptVersion1
 } from "../lib/conditional_statements";
 
 // If statement
 test("If statement", () => {
-  const ver_1 = getJavaScriptVersion("ES5");
-  const ver_2 = getJavaScriptVersion("ES6");
-  const ver_3 = getJavaScriptVersion("ES7");
-  const ver_4 = getJavaScriptVersion("Foo");
+  const ver_1 = getJavaScriptVersion1("ES5");
+  const ver_2 = getJavaScriptVersion1("ES6");
+  const ver_3 = getJavaScriptVersion1("ES7");
+  const ver_4 = getJavaScriptVersion1("Foo");
   expect(ver_1).toBe("ECMAScript 5");
   expect(ver_2).toBe("ECMAScript 2015");
   expect(ver_3).toBe("ECMAScript 2016");
