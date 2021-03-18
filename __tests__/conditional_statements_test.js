2b4bb0304af57abe0472e63149731fc4db83aa15
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-18 12:51:01 +0900

    Commit switch statement

--- __tests__/conditional_statements_test.js
+++ __tests__/conditional_statements_test.js
@@ -1,5 +1,6 @@
 import {
-  getJavaScriptVersion1
+  getJavaScriptVersion1,
+  getJavaScriptVersion2
 } from "../lib/conditional_statements";
 
 // If statement
@@ -8,6 +9,18 @@ test("If statement", () => {
   const ver_2 = getJavaScriptVersion1("ES6");
   const ver_3 = getJavaScriptVersion1("ES7");
   const ver_4 = getJavaScriptVersion1("Foo");
+  expect(ver_1).toBe("ECMAScript 5");
+  expect(ver_2).toBe("ECMAScript 2015");
+  expect(ver_3).toBe("ECMAScript 2016");
+  expect(ver_4).toBe("Input a version of a specific JavaScript version.");
+});
+
+// Switch statement
+test("Switch statement", () => {
+  const ver_1 = getJavaScriptVersion2("ES5");
+  const ver_2 = getJavaScriptVersion2("ES6");
+  const ver_3 = getJavaScriptVersion2("ES7");
+  const ver_4 = getJavaScriptVersion2("Foo");
   expect(ver_1).toBe("ECMAScript 5");
   expect(ver_2).toBe("ECMAScript 2015");
   expect(ver_3).toBe("ECMAScript 2016");
