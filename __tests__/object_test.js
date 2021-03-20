f29da44685785a4ac3385af9c2725a143caf7f80
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-20 16:59:50 +0900

    Commit destructuring assignment

--- __tests__/object_test.js
+++ __tests__/object_test.js
@@ -1,6 +1,8 @@
 import {
   getUserName1,
-  getUserName2
+  getUserName2,
+  getLanguages1,
+  getLanguages2
 } from "../lib/object";
 
 // Access properties
@@ -18,4 +20,29 @@ test("Get username 2", () => {
   }
   const username = getUserName2(user);
   expect(username).toBe("Oasist");
+});
+
+// Destructuring assignment
+test("Get language 1", () => {
+  const langs = {
+    en: "English",
+    ja: "Japanese"
+  }
+  const langs_arr = getLanguages1(langs);
+  const english   = langs_arr[0];
+  const japanese  = langs_arr[1];
+  expect(english).toBe("English");
+  expect(japanese).toBe("Japanese");
+});
+
+test("Get language 2", () => {
+  const langs = {
+    en: "English",
+    ja: "Japanese"
+  }
+  const langs_arr = getLanguages2(langs);
+  const english   = langs_arr[0];
+  const japanese  = langs_arr[1];
+  expect(english).toBe("English");
+  expect(japanese).toBe("Japanese");
 });
