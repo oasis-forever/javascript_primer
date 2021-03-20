5274098c74c87c206b5f8c1cab8ac58261be2214
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-20 15:20:56 +0900

    Add semicolons

--- __tests__/loops_test.js
+++ __tests__/loops_test.js
@@ -20,27 +20,27 @@ test("Add in while 2", () => {
 
 // For
 test("Add in for", () => {
-  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const sum  = addInFor(nums);
   expect(sum).toBe(55);
 });
 
 // ForEach
 test("Add in for", () => {
-  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const sum  = addInForEach(nums);
   expect(sum).toBe(55);
 });
 
 // Break loop
 test("Escape from loop 1", () => {
-  const nums   = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const nums   = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const result = isEvenIncluded1(nums)
   expect(result).toBe(true);
 });
 
 test("Escape from loop 2", () => {
-  const nums   = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const nums   = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const result = isEvenIncluded2(nums)
   expect(result).toBe(true);
 });
