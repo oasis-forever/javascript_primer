376b1bf8260cbb5874a966b35d32406c6c94dfc5
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-20 13:31:27 +0900

    Commit for

--- __tests__/loops_test.js
+++ __tests__/loops_test.js
@@ -1,6 +1,7 @@
 import {
   addInWhile1,
-  addInWhile2
+  addInWhile2,
+  addInFor1
 } from "../lib/loops";
 
 // While
@@ -11,5 +12,12 @@ test("Add in while 1", () => {
 
 test("Add in while 2", () => {
   const sum = addInWhile2();
+  expect(sum).toBe(55);
+});
+
+// For
+test("Add in for 1", () => {
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const sum = addInWhile1(nums);
   expect(sum).toBe(55);
 });
