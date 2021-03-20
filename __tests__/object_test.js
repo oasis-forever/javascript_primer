fcb322488f4f8d610cb734ab8ef62281be53eb6a
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-20 21:39:18 +0900

    Add semicolons

--- __tests__/object_test.js
+++ __tests__/object_test.js
@@ -31,9 +31,9 @@ test("Get language 1", () => {
     en: "English",
     ja: "Japanese"
   }
-  const keys = Object.keys(langs)
-  const vals = Object.values(langs)
-  const entries = Object.entries(langs)
+  const keys = Object.keys(langs);
+  const vals = Object.values(langs);
+  const entries = Object.entries(langs);
   const langsArr = getLanguages1(langs);
   const english   = langsArr[0];
   const japanese  = langsArr[1];
@@ -62,9 +62,9 @@ test("Get language 2", () => {
     en: "English",
     ja: "Japanese"
   }
-  const keys = Object.keys(langs)
-  const vals = Object.values(langs)
-  const entries = Object.entries(langs)
+  const keys = Object.keys(langs);
+  const vals = Object.values(langs);
+  const entries = Object.entries(langs);
   const langsArr = getLanguages2(langs);
   const english   = langsArr[0];
   const japanese  = langsArr[1];
@@ -93,23 +93,24 @@ test("Add property 1", () => {
   const key  = "name";
   const val  = "Oasist";
   const user = addProperty1(key, val);
-  expect(user).toStrictEqual({ name: "Oasist" })
+  expect(user).toStrictEqual({ name: "Oasist" });
 });
 
 test("Add property 2", () => {
   const key  = "name";
   const val  = "Oasist";
   const user = addProperty2(key, val);
-  expect(user).toStrictEqual({ name: "Oasist" })
+  expect(user).toStrictEqual({ name: "Oasist" });
 });
 
 // Optional chaining operator
 test("Get nested property", () => {
   const pLangs = getProgrammingLanguages();
-  const staticLang  = pLangs?.["staticLang"]
-  const dynamicLang = pLangs?.["dynamicLang"]
-  const nonExistent = pLangs?.markup?.foo
-  expect(staticLang).toStrictEqual(["C", "C++", "Java"])
-  expect(dynamicLang).toStrictEqual(["Python", "Python", "PHP", "JavaScript"])
-  expect(nonExistent).toBe(undefined)
+  const staticLang  = pLangs?.["staticLang"];
+  const dynamicLang = pLangs?.["dynamicLang"];
+  const nonExistent = pLangs?.markup?.foo;
+  expect(staticLang).toStrictEqual(["C", "C++", "Java"]);
+  expect(dynamicLang).toStrictEqual(["Python", "Python", "PHP", "JavaScript"]);
+  expect(nonExistent).toBe(undefined);
+});
 });
