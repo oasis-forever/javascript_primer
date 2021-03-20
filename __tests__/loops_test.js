045bcc1de1d44c25e36105381546f1de485d9165
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-20 13:45:55 +0900

    Update loops

--- __tests__/loops_test.js
+++ __tests__/loops_test.js
@@ -1,23 +1,23 @@
 import {
   addInWhile1,
   addInWhile2,
-  addInFor1
+  addInFor
 } from "../lib/loops";
 
 // While
 test("Add in while 1", () => {
-  const sum = addInWhile1();
+  const sum = addInWhile1(0, 10);
   expect(sum).toBe(55);
 });
 
 test("Add in while 2", () => {
-  const sum = addInWhile2();
+  const sum = addInWhile2(0, 10);
   expect(sum).toBe(55);
 });
 
 // For
-test("Add in for 1", () => {
+test("Add in for", () => {
   const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  const sum = addInWhile1(nums);
+  const sum  = addInFor(nums);
   expect(sum).toBe(55);
 });
