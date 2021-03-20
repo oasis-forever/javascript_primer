3d9e504fbc9ed0351cd35545848c3541281d9b63
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-20 15:28:58 +0900

    Comit continue loop

--- __tests__/loops_test.js
+++ __tests__/loops_test.js
@@ -4,7 +4,10 @@ import {
   addInFor,
   addInForEach,
   isEvenIncluded1,
-  isEvenIncluded2
+  isEvenIncluded2,
+  evenNums1,
+  evenNums2,
+  evenNums3
 } from "../lib/loops";
 
 // While
@@ -43,4 +46,23 @@ test("Escape from loop 2", () => {
   const nums   = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const result = isEvenIncluded2(nums)
   expect(result).toBe(true);
+});
+
+// Continue loop
+test("Continue loop 1", () => {
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const evenNumsArray = evenNums1(nums);
+  expect(evenNumsArray).toStrictEqual([2, 4, 6, 8, 10]);
+});
+
+test("Continue loop 2", () => {
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const evenNumsArray = evenNums2(nums);
+  expect(evenNumsArray).toStrictEqual([2, 4, 6, 8, 10]);
+});
+
+test("Continue loop 3", () => {
+  const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const evenNumsArray = evenNums3(nums);
+  expect(evenNumsArray).toStrictEqual([2, 4, 6, 8, 10]);
 });
