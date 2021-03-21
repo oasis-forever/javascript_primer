a6264ac2c4b68f8e810e57268edb9c11363e0079
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-21 23:52:53 +0900

    Commit get strings from a text

--- __tests__/string_test.js
+++ __tests__/string_test.js
@@ -7,4 +7,23 @@ test("Remove duplicate space", () => {
   const newStr = removeDuplicateSpace(str);
   expect(newStr).toBe("a b c d");
 });
+
+test("Get strings from a text", () => {
+  const text = "You said good-bye and I said hello.";
+  const str1 = text.slice(4);
+  const str2 = text.substring(4);
+  expect(str1).toBe("said good-bye and I said hello.");
+  expect(str2).toBe("said good-bye and I said hello.");
+  const str3 = text.slice(2, 8);
+  const str4 = text.substring(2, 8);
+  expect(str3).toBe("u said");
+  expect(str4).toBe("u said");
+  const str5 = text.slice(-1);
+  const str6 = text.substring(-1); // same as text.substring(0);
+  expect(str5).toBe(".");
+  expect(str6).toBe("You said good-bye and I said hello.");
+  const str7 = text.slice(9, 3);
+  const str8 = text.substring(9, 3); // same as text.substring(3, 9);
+  expect(str7).toBe("");
+  expect(str8).toBe(" said ");
 });
