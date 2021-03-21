d34a28a5c9aa48f79a588340538393cdae688f77
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-21 20:44:56 +0900

    Commit index/element reference

--- __tests__/array_test.js
+++ __tests__/array_test.js
@@ -1,6 +1,9 @@
 import {
   getVectorElement,
-  getMatrixElement
+  getMatrixElement,
+  getLangIndex,
+  getSingerIndex,
+  getSingerElement,
 } from "../lib/array";
 
 // Access elememts
@@ -62,4 +65,61 @@ test("Check if the object is an array", () => {
   expect(b).toBe("b");
   expect(c).toBe("c");
   expect(d).toBe("d");
+})
+
+// Index/element reference
+test("Get an index of an array element", () => {
+  const langs   = ["Java", "Python", "Ruby", "JavaScript"];
+  const javaIdx = getLangIndex(langs, "Java");
+  const pyIdx   = getLangIndex(langs, "Python");
+  const rbIdx   = getLangIndex(langs, "Ruby");
+  const jsIdx   = getLangIndex(langs, "JavaScript");
+  const tsIdx   = getLangIndex(langs, "TypeScript");
+  const indexArr1 = langs.slice(1, 3)
+  const indexArr2 = langs.slice(2)
+  const indexArr3 = langs.slice(-1)
+  const indexArr4 = langs.slice(1, 1)
+  const indexArr5 = langs.slice(2, 1)
+  const result_1 = langs.includes("Java")
+  const result_2 = langs.includes("Python")
+  const result_3 = langs.includes("Ruby")
+  const result_4 = langs.includes("JavaScript")
+  const result_5 = langs.includes("TypeScript")
+  expect(javaIdx).toBe(0);
+  expect(pyIdx).toBe(1);
+  expect(rbIdx).toBe(2);
+  expect(jsIdx).toBe(3);
+  expect(tsIdx).toBe(-1);
+  expect(indexArr1).toStrictEqual(["Python", "Ruby"]);
+  expect(indexArr2).toStrictEqual(["Ruby", "JavaScript"]);
+  expect(indexArr3).toStrictEqual(["JavaScript"]);
+  expect(indexArr4).toStrictEqual([]);
+  expect(indexArr5).toStrictEqual([]);
+  expect(result_1).toBe(true);
+  expect(result_2).toBe(true);
+  expect(result_3).toBe(true);
+  expect(result_4).toBe(true);
+  expect(result_5).toBe(false);
+});
+
+test("Get an index object elements", () => {
+  const singers = [
+    { name: "Noel Gallagher" },
+    { name: "Liam Gallagher" },
+    { name: "Michal Jackson" },
+    { name: "Freddie Mercury" }
+  ]
+  const noelIdx = getSingerIndex(singers);
+  expect(noelIdx).toBe(0);
+});
+
+test("Get an element of object elements", () => {
+  const singers = [
+    { name: "Noel Gallagher" },
+    { name: "Liam Gallagher" },
+    { name: "Michal Jackson" },
+    { name: "Freddie Mercury" }
+  ]
+  const noelIdx = getSingerElement(singers);
+  expect(noelIdx).toStrictEqual({ name: "Noel Gallagher" });
 });
