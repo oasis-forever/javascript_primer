382802419d1a845b85ef4d4e5db655b6167ea246
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-22 22:42:16 +0900

    Commit userTitle

--- __tests__/functions_and_this_test.js
+++ __tests__/functions_and_this_test.js
@@ -1,6 +1,8 @@
 import {
   getGreetingMessage,
-  greet
+  greet,
+  userTitle1,
+  userTitle2
 } from "../lib/functions_and_this";
 
 test("Greeting 1", () => {
@@ -17,4 +19,11 @@ test("Greeting 2", () => {
   expect(greeting_1).toBe("Hi, I'm Noel Gallagher. Nice to see you!");
   expect(greeting_2).toBe("Hi, I'm Noel Gallagher. Nice to see you!");
   expect(greeting_3()).toBe("Hi, I'm Noel Gallagher. Nice to see you!");
+});
+
+test("Users with a title", () => {
+  const users_1 = userTitle1.addTitle(["Noel Gallagher", "Liam Gallagher"])
+  const users_2 = userTitle2.addTitle(["Noel Gallagher", "Liam Gallagher"])
+  expect(users_1).toStrictEqual(["Mr. Noel Gallagher", "Mr. Liam Gallagher"])
+  expect(users_2).toStrictEqual(["Mr. Noel Gallagher", "Mr. Liam Gallagher"])
 });
