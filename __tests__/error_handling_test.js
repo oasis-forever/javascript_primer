f0dcbf41fcf7bec20b66384568e8983808f3bc99
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-25 13:58:28 +0900

    Commit errorHandler2

--- __tests__/error_handling_test.js
+++ __tests__/error_handling_test.js
@@ -1,9 +1,15 @@
 import {
-  handleError1
+  handleError1,
+  handleError2
 } from "../lib/error_handling";
 
 test("handleError1", () => {
   const error = handleError1();
   expect(error instanceof ReferenceError).toBe(true);
   expect(error.message).toBe("undefinedFunction is not defined");
+});
+
+test("handleError2", () => {
+  const message = handleError2();
+  expect(message).toBe("This line is executed every time.");
 });
