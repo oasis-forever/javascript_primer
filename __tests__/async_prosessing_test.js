4259ce14460d644ea5845faa0d16e96eeed9f57f
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-27 16:20:06 +0900

    Commit getAsyncProcesses

--- __tests__/async_prosessing_test.js
+++ __tests__/async_prosessing_test.js
@@ -1,9 +1,22 @@
 import {
-  blockTime
+  blockTime,
+  getAsyncProcesses
 } from "../lib/async_prosessing";
 
 test("blockTime", () => {
   const timeout = 1000;
   const message = blockTime(timeout);
   expect(message).toBe(`${timeout} millisecond seconds have passed.`);
+});
+
+test("getAsyncProcesses", () => {
+  const timeout   = 1000;
+  const millisec  = 10;
+  const processes = getAsyncProcesses(timeout, millisec);
+  expect(processes).toStrictEqual([
+    `A callback func will start in ${millisec} milliseconds.`,
+    "A sysc process will start now.",
+    // "The blocking process is starting now.",
+    // "The blocking process is done."
+  ]);
 });
