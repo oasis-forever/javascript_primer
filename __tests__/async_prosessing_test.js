55e758504b9667006baeadf9b7dc8fc08d2ebfbf
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-27 18:42:55 +0900

    Rename function

--- __tests__/async_prosessing_test.js
+++ __tests__/async_prosessing_test.js
@@ -2,7 +2,7 @@
 
 import {
   blockTime,
-  getAsyncProcesses
+  getAsyncProcesses1,
 } from "../lib/async_prosessing";
 
 test("blockTime", () => {
@@ -11,10 +11,10 @@ test("blockTime", () => {
   expect(message).toBe(`${timeout} millisecond seconds have passed.`);
 });
 
-test("getAsyncProcesses", () => {
+test("getAsyncProcesses1", () => {
   const timeout   = 1000;
   const millisec  = 10;
-  const processes = getAsyncProcesses(timeout, millisec);
+  const processes = getAsyncProcesses1(timeout, millisec);
   expect(processes).toStrictEqual([
     `A callback func will start in ${millisec} milliseconds.`,
     "A sysc process will start now.",
