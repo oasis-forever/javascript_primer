8ab4b63bd55462d72143ef8092d8b30122815a0c
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-30 00:14:31 +0900

    Commit setMap

--- __tests__/map_and_set_test.js
+++ __tests__/map_and_set_test.js
@@ -1,7 +1,8 @@
 "use strict;"
 
 import {
-  newMap
+  newMap,
+  setMap
 } from "../lib/map_and_set";
 
 test("newMap", () => {
@@ -12,4 +13,24 @@ test("newMap", () => {
   const map = newMap(entry);
   expect(map.get("key_1")).toBe("value_1");
   expect(map.get("key_2")).toBe("value_2");
+});
+
+test("setMap", () => {
+  const map = setMap();
+  expect(map.has(undefined)).toBe(true);
+  expect(map.get(undefined)).toBe(undefined);
+  map.set("Foo", "Bar");
+  expect(map.has("Foo")).toBe(true);
+  expect(map.get("Foo")).toBe("Bar");
+  map.set("Hoge", "Piyo");
+  expect(map.has("Hoge")).toBe(true);
+  expect(map.get("Hoge")).toBe("Piyo");
+  map.set("Hoge", "FooBarPiyo");
+  expect(map.get("Hoge")).toBe("FooBarPiyo");
+  map.delete(undefined);
+  expect(map.has(undefined)).toBe(false);
+  map.delete("Foo");
+  expect(map.has("Foo")).toBe(false);
+  map.delete("Hoge");
+  expect(map.has("Hoge")).toBe(false);
 });
