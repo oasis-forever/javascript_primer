e6f4e171c74fbcb053ba8cbd36519f6ed7541a70
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-31 01:47:32 +0900

    Commit getMin

--- __tests__/math_test.js
+++ __tests__/math_test.js
@@ -2,7 +2,8 @@
 
 import {
   getRandom,
-  getMax
+  getMax,
+  getMin
 } from "../lib/math";
 
 test("getRandom", () => {
@@ -14,4 +15,10 @@ test("getMax", () => {
   const nums = [...Array(10).keys()].map(num => ++num);
   const max  = getMax(nums);
   expect(max).toBe(10);
+});
+
+test("getMin", () => {
+  const nums = [...Array(10).keys()].map(num => ++num);
+  const max  = getMin(nums);
+  expect(max).toBe(1);
 });
