c8640b1e78e7d9d001f1e303d8d5445da3fdbf90
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-03-31 01:45:01 +0900

    Commit getMax

--- __tests__/math_test.js
+++ __tests__/math_test.js
@@ -1,10 +1,17 @@
 "use strict;"
 
 import {
-  getRandom
+  getRandom,
+  getMax
 } from "../lib/math";
 
 test("getRandom", () => {
   const random = getRandom(10, 0);
   expect(typeof random).toBe("number");
+});
+
+test("getMax", () => {
+  const nums = [...Array(10).keys()].map(num => ++num);
+  const max  = getMax(nums);
+  expect(max).toBe(10);
 });
